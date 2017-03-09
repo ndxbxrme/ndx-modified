da2e54988d75992170047f15c917ccb5d9c6ede5
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-09 18:12:18 +0000

    ndx not args

--- build/index.js
+++ build/index.js
@@ -3,18 +3,18 @@
   module.exports = function(ndx) {
     ndx.database.on('preInsert', function(args) {
       if (args.obj) {
-        args.obj[process.env.INSERT_FIELD || args.settings.INSERT_FIELD || process.env.MODIFIED_FIELD || args.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
-        args.obj[process.env.INSERT_USER_FIELD || args.settings.INSERT_USER_FIELD || process.env.MODIFIED_USER_FIELD || args.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
-        args.obj[process.env.MODIFIED_FIELD || args.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
-        return args.obj[process.env.MODIFIED_USER_FIELD || args.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
+        args.obj[process.env.INSERT_FIELD || ndx.settings.INSERT_FIELD || process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
+        args.obj[process.env.INSERT_USER_FIELD || ndx.settings.INSERT_USER_FIELD || process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
+        args.obj[process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
+        return args.obj[process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
       }
     });
     return ndx.database.on('preUpdate', function(args) {
       if (args.obj) {
-        args.obj[process.env.INSERT_FIELD || args.settings.INSERT_FIELD || process.env.MODIFIED_FIELD || args.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
-        args.obj[process.env.INSERT_USER_FIELD || args.settings.INSERT_USER_FIELD || process.env.MODIFIED_USER_FIELD || args.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
-        args.obj[process.env.MODIFIED_FIELD || args.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
-        return args.obj[process.env.MODIFIED_USER_FIELD || args.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
+        args.obj[process.env.INSERT_FIELD || ndx.settings.INSERT_FIELD || process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
+        args.obj[process.env.INSERT_USER_FIELD || ndx.settings.INSERT_USER_FIELD || process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
+        args.obj[process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
+        return args.obj[process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
       }
     });
   };
