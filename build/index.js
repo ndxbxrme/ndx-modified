2e5b716bef7a3f31558d1527b198eb1f00762a6e
ndxbxrme <lewis_the_cat@hotmail.com>
2017-04-16 15:54:02 +0100

    callback callbacks

--- build/index.js
+++ build/index.js
@@ -1,25 +1,27 @@
 (function() {
   'use strict';
   module.exports = function(ndx) {
-    ndx.database.on('preInsert', function(args) {
+    ndx.database.on('preInsert', function(args, cb) {
       if (args.obj) {
         args.obj[process.env.INSERT_FIELD || ndx.settings.INSERT_FIELD || process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
         args.obj[process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
         if (ndx.user) {
           args.obj[process.env.INSERT_USER_FIELD || ndx.settings.INSERT_USER_FIELD || process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
-          return args.obj[process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
+          args.obj[process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
         }
       }
+      return cb();
     });
-    return ndx.database.on('preUpdate', function(args) {
+    return ndx.database.on('preUpdate', function(args, cb) {
       if (args.obj) {
         args.obj[process.env.INSERT_FIELD || ndx.settings.INSERT_FIELD || process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
         args.obj[process.env.MODIFIED_FIELD || ndx.settings.MODIFIED_FIELD || 'modifiedAt'] = new Date().valueOf();
         if (ndx.user) {
           args.obj[process.env.INSERT_USER_FIELD || ndx.settings.INSERT_USER_FIELD || process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
-          return args.obj[process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
+          args.obj[process.env.MODIFIED_USER_FIELD || ndx.settings.MODIFIED_USER_FIELD || 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID];
         }
       }
+      return cb();
     });
   };
 
--- src/index.coffee
+++ src/index.coffee
@@ -1,18 +1,20 @@
 'use strict'
 
 module.exports = (ndx) ->
-  ndx.database.on 'preInsert', (args) ->
+  ndx.database.on 'preInsert', (args, cb) ->
     if args.obj
       args.obj[process.env.INSERT_FIELD or ndx.settings.INSERT_FIELD or process.env.MODIFIED_FIELD or ndx.settings.MODIFIED_FIELD or 'modifiedAt'] = new Date().valueOf()
       args.obj[process.env.MODIFIED_FIELD or ndx.settings.MODIFIED_FIELD or 'modifiedAt'] = new Date().valueOf()
       if ndx.user
         args.obj[process.env.INSERT_USER_FIELD or ndx.settings.INSERT_USER_FIELD or process.env.MODIFIED_USER_FIELD or ndx.settings.MODIFIED_USER_FIELD or 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID]
         args.obj[process.env.MODIFIED_USER_FIELD or ndx.settings.MODIFIED_USER_FIELD or 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID]
-  ndx.database.on 'preUpdate', (args) ->
+    cb()
+  ndx.database.on 'preUpdate', (args, cb) ->
     if args.obj
       args.obj[process.env.INSERT_FIELD or ndx.settings.INSERT_FIELD or process.env.MODIFIED_FIELD or ndx.settings.MODIFIED_FIELD or 'modifiedAt'] = new Date().valueOf()
       args.obj[process.env.MODIFIED_FIELD or ndx.settings.MODIFIED_FIELD or 'modifiedAt'] = new Date().valueOf()
       if ndx.user
         args.obj[process.env.INSERT_USER_FIELD or ndx.settings.INSERT_USER_FIELD or process.env.MODIFIED_USER_FIELD or ndx.settings.MODIFIED_USER_FIELD or 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID]
         args.obj[process.env.MODIFIED_USER_FIELD or ndx.settings.MODIFIED_USER_FIELD or 'modifiedBy'] = ndx.user[ndx.settings.AUTO_ID]
+    cb()
       
